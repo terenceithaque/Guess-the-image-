b7e9510a8b938830402dc02f4feb5b8bb5e55467
terenceithaque <terence.lethierry@protonmail.com>
2023-08-04 20:02:17 +0200

    Ajouté la fonction pick_images

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # Programme principal du jeu
+import random
+import os
 import pygame  # Importation du module Pygame
 pygame.init()  # Démarrer Pygame
 
 screen = pygame.display.set_mode(size=(400, 400))  # Fenêtre du jeu
 pygame.display.set_caption("Guess the image !")
 
+dossier_images = "images"  # Dossier des images
+liste_images = os.listdir(dossier_images)
+
+
+def pick_images(images_to_pick):
+    "Choisir plusieurs images au hasard dans liste_images"
+    n_images_choisies = 0
+    images_choisies = []  # Pour l'instant, aucune image n'a été choisie
+    while n_images_choisies < images_to_pick:  # Tant qu'on n'a pas choisi autant d'images que demandé
+        image = random.choice(liste_images)
+        images_choisies.append(image)
+        n_images_choisies += 1
+
+    return images_choisies
+
+
+images = pick_images(1)
+print(images)
 running = True  # Le jeu est-il en cours d'exécution ?
 
 
