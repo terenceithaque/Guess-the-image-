e7da46d6a46b6873a6823d9eb6e0f056e72f9d1c
terenceithaque <terence.lethierry@protonmail.com>
2023-08-05 11:28:43 +0200

    Jeu capable d'afficher des images

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame  # Importation du module Pygame
 from tkinter import messagebox
 pygame.init()  # Démarrer Pygame
 
-screen = pygame.display.set_mode(size=(400, 400))  # Fenêtre du jeu
+screen = pygame.display.set_mode(size=(800, 800))  # Fenêtre du jeu
 pygame.display.set_caption("Guess the image !")
 
 dossier_images = "images"  # Dossier des images
@@ -21,13 +21,24 @@ def pick_images(images_to_pick):
     images_choisies = []  # Pour l'instant, aucune image n'a été choisie
     while n_images_choisies < images_to_pick:  # Tant qu'on n'a pas choisi autant d'images que demandé
         image = random.choice(liste_images)
+        if image in images_choisies:  # Si on a déjà choisi la même image
+            continue
+
+        print(image)
         images_choisies.append(image)
         n_images_choisies += 1
-
     return images_choisies
 
 
-images = pick_images(1)
+def display_images_and_choices(image, choices):
+    "Afficher chaque image et les choix de réponse correspondants"
+    # Charger et afficher l'image
+    chemin_image = os.path.join(dossier_images, image)
+    image_chargee = pygame.image.load(chemin_image)
+    screen.blit(image_chargee, (0, 0))  # Afficher l'image à l'écran
+
+
+images = pick_images(2)
 print(images)
 running = True  # Le jeu est-il en cours d'exécution ?
 
@@ -36,3 +47,11 @@ while running:
     for evenement in pygame.event.get():  # Pour chaque évènement détecté dans la fenêtre de jeu
         if evenement == pygame.QUIT:  # Si le joueur veut quitter le jeu
             running = False  # Terminer l'exécution de cette boucle, et, par conséquent, celle du jeu
+
+        for image in images:
+            display_images_and_choices(
+                image, ["Choix 1", "Choix 2", "Choix 3"]
+
+            )
+
+    pygame.display.update()
